7b05eec16f94b69da440558ac37db7a80e2d5e29
kazumakishimoto <kazuma.kishimoto0114@gmail.com>
2022-01-30 20:46:52 +0900

    fix_validation #15

--- src/app/Http/Controllers/Auth/RegisterController.php
+++ src/app/Http/Controllers/Auth/RegisterController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Socialite\Facades\Socialite;
+use App\Rules\CustomPasswordValidation;
 
 class RegisterController extends Controller
 {
--- src/app/Rules/CustomPasswordValidation.php
+++ src/app/Rules/CustomPasswordValidation.php
@@ -4,7 +4,7 @@ namespace App\Rules;
 
 use Illuminate\Contracts\Validation\Rule;
 
-class PasswordValidation implements Rule
+class CustomPasswordValidation implements Rule
 {
     //最小文字数
     private const MIN_CHARACTER = 8;
