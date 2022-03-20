89ed84e3088673a47f5d265dfd6a9e1b8a98ec84
kazumakishimoto <kazuma.kishimoto0114@gmail.com>
2022-03-20 11:28:32 +0900

    Add Procfile, varcher length, force https #42

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -23,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
+        
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
