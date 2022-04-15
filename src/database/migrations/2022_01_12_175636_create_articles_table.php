f30b0846a974a2bfab2b2fc78f748e3599d25692
kazumakishimoto <kazuma.kishimoto0114@gmail.com>
2022-04-15 13:18:05 +0900

    fix search #36

--- src/database/migrations/2022_01_12_175636_create_articles_table.php
+++ src/database/migrations/2022_01_12_175636_create_articles_table.php
@@ -16,7 +16,7 @@ class CreateArticlesTable extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->bigInteger('user_id')->unsigned();
-            $table->bigInteger('pref_id');
+            $table->bigInteger('pref_id')->nullable();
             $table->text('title');
             $table->text('body');
             $table->string('image')->nullable();
