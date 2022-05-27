86e2bb2cd27894975cf28ddf4fc38aa51befb937
kazumakishimoto <kazuma.kishimoto0114@gmail.com>
2022-05-27 19:58:20 +0900

    fix https #36

--- src/app/Http/Middleware/ForceHttps.php
+++ src/app/Http/Middleware/ForceHttps.php
@@ -15,7 +15,7 @@ class ForceHttps
      */
     public function handle($request, Closure $next)
     {
-        if (\App::environment(['production']) && $_SERVER["HTTP_X_FORWARDED_PROTO"] != 'https') {
+        if (\App::environment(['production']) && isset($_SERVER["HTTP_X_FORWARDED_PROTO"]) != 'https') {
             return redirect()->secure($request->getRequestUri());
         }
         return $next($request);
